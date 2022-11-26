f4466be91c093d01acc3e699f01ba66dd0f66e26
Andrius Petrauskis <andrius@petrauskis.com>
2022-11-26 23:05:08 +0200

    Refactor ProductUrlProvider to use AbstractBatchableResourceUrlProvider
    This greatly simplifies code for the specific provider and adds ability to generate product sitemaps in batches.

--- src/Provider/ProductUrlProvider.php
+++ src/Provider/ProductUrlProvider.php
@@ -4,39 +4,17 @@ declare(strict_types=1);
 
 namespace SitemapPlugin\Provider;
 
-use Doctrine\Common\Collections\Collection;
 use SitemapPlugin\Factory\AlternativeUrlFactoryInterface;
 use SitemapPlugin\Factory\UrlFactoryInterface;
-use SitemapPlugin\Generator\ProductImagesToSitemapImagesCollectionGeneratorInterface;
-use SitemapPlugin\Model\ChangeFrequency;
-use SitemapPlugin\Model\UrlInterface;
+use SitemapPlugin\Generator\ResourceImagesToSitemapImagesCollectionGeneratorInterface;
 use Sylius\Bundle\CoreBundle\Doctrine\ORM\ProductRepository;
 use Sylius\Component\Core\Model\ChannelInterface;
-use Sylius\Component\Core\Model\ProductInterface;
-use Sylius\Component\Core\Model\ProductTranslationInterface;
 use Sylius\Component\Locale\Context\LocaleContextInterface;
-use Sylius\Component\Locale\Model\LocaleInterface;
-use Sylius\Component\Resource\Model\TranslationInterface;
 use Symfony\Component\Routing\RouterInterface;
 
-final class ProductUrlProvider implements UrlProviderInterface
+final class ProductUrlProvider extends AbstractBatchableResourceUrlProvider
 {
     private ProductRepository $productRepository;
-
-    private RouterInterface $router;
-
-    private UrlFactoryInterface $urlFactory;
-
-    private AlternativeUrlFactoryInterface $urlAlternativeFactory;
-
-    private LocaleContextInterface $localeContext;
-
-    private ChannelInterface $channel;
-
-    /** @var array<string|null> */
-    private array $channelLocaleCodes;
-
-    private ProductImagesToSitemapImagesCollectionGeneratorInterface $productToImageSitemapArrayGenerator;
 
     public function __construct(
         ProductRepository $productRepository,
@@ -44,14 +22,16 @@ final class ProductUrlProvider implements UrlProviderInterface
         UrlFactoryInterface $urlFactory,
         AlternativeUrlFactoryInterface $urlAlternativeFactory,
         LocaleContextInterface $localeContext,
-        ProductImagesToSitemapImagesCollectionGeneratorInterface $productToImageSitemapArrayGenerator
+        ResourceImagesToSitemapImagesCollectionGeneratorInterface $resourceToImageSitemapArrayGenerator
     ) {
+        parent::__construct(
+            $router,
+            $urlFactory,
+            $urlAlternativeFactory,
+            $localeContext,
+            $resourceToImageSitemapArrayGenerator
+        );
         $this->productRepository = $productRepository;
-        $this->router = $router;
-        $this->urlFactory = $urlFactory;
-        $this->urlAlternativeFactory = $urlAlternativeFactory;
-        $this->localeContext = $localeContext;
-        $this->productToImageSitemapArrayGenerator = $productToImageSitemapArrayGenerator;
     }
 
     public function getName(): string
@@ -59,98 +39,36 @@ final class ProductUrlProvider implements UrlProviderInterface
         return 'products';
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function generate(ChannelInterface $channel): iterable
+    protected function getRouteName(): string
     {
-        $this->channel = $channel;
-        $urls = [];
-        $this->channelLocaleCodes = [];
-
-        foreach ($this->getProducts() as $product) {
-            $urls[] = $this->createProductUrl($product);
-        }
-
-        return $urls;
+        return 'sylius_shop_product_show';
     }
 
-    private function getTranslations(ProductInterface $product): Collection
-    {
-        return $product->getTranslations()->filter(function (TranslationInterface $translation): bool {
-            return $this->localeInLocaleCodes($translation);
-        });
-    }
-
-    private function localeInLocaleCodes(TranslationInterface $translation): bool
-    {
-        return \in_array($translation->getLocale(), $this->getLocaleCodes(), true);
-    }
-
-    /**
-     * @return array|Collection|ProductInterface[]
-     */
-    private function getProducts(): iterable
+    protected function getResources(ChannelInterface $channel, int $start, int $entries): iterable
     {
         return $this->productRepository->createQueryBuilder('o')
             ->addSelect('translation')
             ->innerJoin('o.translations', 'translation')
             ->andWhere(':channel MEMBER OF o.channels')
             ->andWhere('o.enabled = :enabled')
-            ->setParameter('channel', $this->channel)
+            ->setParameter('channel', $channel)
             ->setParameter('enabled', true)
+            ->setFirstResult($start)
+            ->setMaxResults($entries)
             ->getQuery()
             ->getResult();
     }
 
-    private function getLocaleCodes(): array
+    protected function count(ChannelInterface $channel): int
     {
-        if ($this->channelLocaleCodes === []) {
-            $this->channelLocaleCodes = $this->channel->getLocales()->map(function (LocaleInterface $locale): ?string {
-                return $locale->getCode();
-            })->toArray();
-        }
-
-        return $this->channelLocaleCodes;
-    }
-
-    private function createProductUrl(ProductInterface $product): UrlInterface
-    {
-        $productUrl = $this->urlFactory->createNew(''); // todo bypassing this new constructor right now
-        $productUrl->setChangeFrequency(ChangeFrequency::always());
-        $productUrl->setPriority(0.5);
-        $updatedAt = $product->getUpdatedAt();
-        if ($updatedAt !== null) {
-            $productUrl->setLastModification($updatedAt);
-        }
-        $productUrl->setImages($this->productToImageSitemapArrayGenerator->generate($product));
-
-        /** @var ProductTranslationInterface $translation */
-        foreach ($this->getTranslations($product) as $translation) {
-            $locale = $translation->getLocale();
-
-            if ($locale === null) {
-                continue;
-            }
-
-            if (!$this->localeInLocaleCodes($translation)) {
-                continue;
-            }
-
-            $location = $this->router->generate('sylius_shop_product_show', [
-                'slug' => $translation->getSlug(),
-                '_locale' => $translation->getLocale(),
-            ]);
-
-            if ($locale === $this->localeContext->getLocaleCode()) {
-                $productUrl->setLocation($location);
-
-                continue;
-            }
-
-            $productUrl->addAlternative($this->urlAlternativeFactory->createNew($location, $locale));
-        }
-
-        return $productUrl;
+        return $this->productRepository->createQueryBuilder('o')
+            ->select('count(*)')
+            ->innerJoin('o.translations', 'translation')
+            ->andWhere(':channel MEMBER OF o.channels')
+            ->andWhere('o.enabled = :enabled')
+            ->setParameter('channel', $channel)
+            ->setParameter('enabled', true)
+            ->getQuery()
+            ->getSingleColumnResult();
     }
 }
